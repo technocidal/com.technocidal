58c5555ea09fe23ad10257e3561c22cb8d8f0656
Johannes Ebeling <14994778+technocidal@users.noreply.github.com>
2023-06-05 14:47:58 +0200

    ✨ Move deployment into this repository

--- deployment/pass.sh
+++ deployment/pass.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-# Keychain query fields.
-# LABEL is the value you put for "Keychain Item Name" in Keychain.app.
-LABEL="technocidal-cloud-vault"
-ACCOUNT_NAME="technocidal"
-
-/usr/bin/security find-generic-password -w -a "$ACCOUNT_NAME" -l "$LABEL"se
+op read "op://Software/deploy-technocidal-com/password"
